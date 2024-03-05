424e3a34c3c9f9b3a3e8ec6f80bd8ace79c8bc20
Vedant <vedant0711@gmail.com>
2024-03-06 01:00:43 +0530

    Cpnnect

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
 const app =express();
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb+srv://vedant0711:Vedant0711@vedant.wedaith.mongodb.net/?retryWrites=true&w=majority&appName=Vedant").then(
+    ()=>{
+        console.log("connected to mongodb");
+    }
+).catch((err)=>{
+    console.log(err);
+})
 app.listen(8086,()=>{
 console.log("Server is listening on 8086!");
 }
