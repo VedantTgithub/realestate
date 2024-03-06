a99f03ecb19f5aaf176eebb520b1c3af368e74d0
Vedant <vedant0711@gmail.com>
2024-03-07 00:02:45 +0530

    authentication

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const app =express();
+app.use(express.json());
 import mongoose from 'mongoose';
-
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 mongoose.connect("mongodb+srv://vedant0711:Vedant0711@vedant.wedaith.mongodb.net/?retryWrites=true&w=majority&appName=Vedant").then(
     ()=>{
         console.log("connected to mongodb");
@@ -13,3 +15,6 @@ app.listen(8086,()=>{
 console.log("Server is listening on 8086!");
 }
 );
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
