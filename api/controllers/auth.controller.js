3487eafe0f727b5ef258f9a04d14380a5891ee41
Vedant <vedant0711@gmail.com>
2024-03-16 16:40:25 +0530

    new commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,31 @@
 import { model } from "mongoose";
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
-export const signup=async(req,res)=>{
-   const {username,email,password}=req.body;
-   const hashedPassword=bcryptjs.hashSync(password,10);
-   const newuser=new User({username,email,password:hashedPassword});
-  try{ 
-    await newuser.save();
-   res.status(201).json("User created!");
-  }
-  catch(error)
-  {
-    res.status(500).json(error);
-  }
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async (req, res) => {
+   const { username, email, password } = req.body;
+
+   try {
+      // Check if password is provided
+      if (!password) {
+         return res.status(400).json({ error: 'Password is required' });
+      }
+
+      // Hash the password
+      const hashedPassword = bcryptjs.hashSync(password, 10);
+
+      // Create a new user instance
+      const newUser = new User({ username, email, password: hashedPassword });
+
+      // Save the new user to the database
+      await newUser.save();
+
+      // Respond with success message
+      res.status(201).json({ message: "User created successfully" });
+   } catch (error) {
+      // Handle errors
+      console.error("Error creating user:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+   }
 };
--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+
 const app =express();
 app.use(express.json());
+app.use(cors());
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
@@ -18,3 +21,12 @@ console.log("Server is listening on 8086!");
 
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
