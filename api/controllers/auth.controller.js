39b82e8a27ed66ea5ba9f3557d62f1569853b30b
Vedant <vedant0711@gmail.com>
2024-03-18 00:36:56 +0530

    redux

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { model } from "mongoose";
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res) => {
    const { username, email, password } = req.body;
@@ -28,4 +29,24 @@ export const signup = async (req, res) => {
       console.error("Error creating user:", error);
       res.status(500).json({ error: "Internal Server Error" });
    }
+
 };
+
+export const signin = async (req, res, next) => {
+   const { email, password } = req.body;
+   try {
+     const validUser = await User.findOne({ email });
+     if (!validUser) return next(errorHandler(404, 'User not found!'));
+     const validPassword = bcryptjs.compareSync(password, validUser.password);
+     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+     const token = jwt.sign({ id: validUser._id }, 'Vedant123');
+     const { password: pass, ...rest } = validUser._doc;
+     res
+       .cookie('access_token', token, { httpOnly: true })
+       .status(200)
+       .json(rest);
+   } catch (error) {
+     next(error);
+   }
+ };
+
