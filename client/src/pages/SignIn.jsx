1aa575317129c8f8201ec6d1cc31d3ede6e75714
Vedant <vedant0711@gmail.com>
2024-03-20 01:03:14 +0530

    LAte night sleep

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -9,6 +9,7 @@ import {
 } from '../redux/user/userSlice';
 import OAuth from '../components/OAuth';
 
+
 export default function SignIn() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
@@ -73,7 +74,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
-        <OAuth/>
+       <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Don't Have an account?</p>
