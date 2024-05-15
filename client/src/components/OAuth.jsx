aa5d782381f07c277ecdbdb79ccace7d822d8e8d
Vedant <vedant0711@gmail.com>
2024-05-16 01:04:26 +0530

    Fixed Sign In

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import { app} from '../firebase';
-import {GoogleAuthProvider,getAuth,signInWithPopup} from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice.js';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'; // Import Axios
 
-const handleGoogleClick = async () => {
-  try {
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth(app);
+export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
 
-    const result = await signInWithPopup(auth, provider);
-    console.log(result);
-  }
-  catch (error) {
-    console.log('could not sign in with google');
-  }
-};
+      const result = await signInWithPopup(auth, provider);
 
+      const response = await axios.post('/api/auth/google', {
+        name: result.user.displayName,
+        email: result.user.email,
+        photo: result.user.photoURL,
+      });
 
-function OAuth() {
+      const data = response.data;
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log('Could not sign in with Google', error);
+    }
+  };
+
   return (
-    <button onClick={handleGoogleClick}
-    type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Continue With Google</button>
-  )
+    <button
+      onClick={handleGoogleClick}
+      type='button'
+      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+    >
+      Continue with Google
+    </button>
+  );
 }
-
-export default OAuth;
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,7 +6,7 @@ import {
   signInStart,
   signInSuccess,
   signInFailure,
-} from '../redux/user/userSlice';
+} from '../redux/user/userSlice.js';
 import OAuth from '../components/OAuth';
 
 
@@ -50,7 +50,7 @@ export default function SignIn() {
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
        
         <input
